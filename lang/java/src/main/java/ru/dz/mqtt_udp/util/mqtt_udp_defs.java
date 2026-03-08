20201c71bf7c3eb6f6d88c0c07a4b7b8a9d77547
pmanilov <pavman0123456789@gmail.com>
2026-03-08 15:41:06 +0300

    feat: Increase MQTT-UDP packet buffer size.

--- lang/java/src/main/java/ru/dz/mqtt_udp/util/mqtt_udp_defs.java
+++ lang/java/src/main/java/ru/dz/mqtt_udp/util/mqtt_udp_defs.java
@@ -19,7 +19,7 @@ public class mqtt_udp_defs {
     public static final int PTYPE_PINGRESP = 0xD0;
     public static final int PTYPE_DISCONNECT = 0xE0;
     public static final int MQTT_PORT = 1883;
-    public static final int PKT_BUF_SIZE = 4096;
+    public static final int PKT_BUF_SIZE = 65535;
     public static final int PKT_PACE_MSEC = 50;
     public static final int DEFAULT_MAX_REPLY_QOS = 1;
     public static final String SYS_CONF_PREFIX = "$SYS/conf";
